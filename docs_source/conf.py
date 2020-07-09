7872b5de3bc2e3019cd78fbc6582948d5bcf920b
Joshua Dawes <joshua.dawes@cern.ch>
2020-07-09 23:39:43 +0200

    Update conf.py

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-print(os.listdir("/home/docs/checkouts/readthedocs.org/user_builds/vypranalysis/"))
+print(os.listdir("/home/docs/checkouts/readthedocs.org/user_builds/vypranalysis/checkouts/"))
 
 
 # -- Project information -----------------------------------------------------
